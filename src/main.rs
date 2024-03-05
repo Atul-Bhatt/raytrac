4d7de8342f3e5f4ccda76e98af775aec29e71c6e
Atul Bhatt <atulbhatt61@gmail.com>
2024-03-05 09:43:09 +0530

    add operator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod point;
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
--- src/point.rs
+++ src/point.rs
@@ -2,13 +2,38 @@
 struct Point(f64, f64, f64);
 
 impl Point {
-    fn x(&self) -> i64 {
+    fn new() -> Point {
+        Point(0.0,0.0,0.0)
+    }
+    fn x(&self) -> f64 {
         self.0
     }
-    fn y(&self) -> i64 {
+    fn y(&self) -> f64 {
         self.1
     }
-    fn z(&self) -> i64 {
+    fn z(&self) -> f64 {
         self.2
     }
+    // unary negative
+    
+   
+
+    // multiplication
+    // division
+    // length
 }
+// addition
+pub trait Add<RHS = Self> {
+    type Output;
+    
+    fn add(self, rhs: RHS) -> Self::Output;
+}
+
+impl Add<Point> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Point) -> Point {
+        Point(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    }
+}
+
